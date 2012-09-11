11c05761591b19a912a9bf8c8405220b112f7c54
Uvarov Michael <freeakk@gmail.com>
2012-09-11 13:03:02 +0400

    Add tests.

--- src/sangria.erl
+++ src/sangria.erl
@@ -3,7 +3,7 @@
 
 parse_transform(Forms, _Options) ->
     F = fun search_and_transform_function_call_with_any/1,
-    X = [erl_syntax_lib:map(F, Tree) || Tree <- Forms],
+    X = [erl_syntax:revert(erl_syntax_lib:map(F, Tree)) || Tree <- Forms],
 %   io:format(user, "Before:\t~p\n\nAfter:\t~p\n", [Forms, X]),
     X.
 
@@ -12,45 +12,18 @@ search_and_transform_function_call_with_any(Node) ->
         true ->
             {RecNames, Action, AltAction}
                  = decode_arguments(erl_syntax:application_arguments(Node)),
-            F = search_and_transform_record_access_any(RecNames, AltAction),
+            F = search_and_transform_record_action_any(RecNames, AltAction),
             erl_syntax_lib:map(F, Action);
         false -> Node
     end.
 
 decode_arguments([RecNames, Action | Args]) ->
-    RecNameValues = decode_record_names(RecNames),
+    RecNameValues = erl_syntax:list_elements(RecNames),
     AltAction = maybe_optional_argument(Args),
     {RecNameValues, Action, AltAction}.
 
-search_and_transform_record_access_any(RecNameValues, AltAction) ->
-    fun(Node) ->
-            case node_type(Node) of
-                record_expr -> %% X#node{f1 = Y}
-                    Node;
-                record_access -> %% X#node.f1
-                    handle_record_access_any(RecNameValues, AltAction, Node);
-                    Node;
-                _OtherType -> io:format(user, "~nOtherType: ~p~n", [_OtherType])
-            end
-        end.
-
-handle_record_access_any(RecNameValues, AltAction, Node) ->
-    RecType = erl_syntax:atom_value(erl_syntax:record_expr_type(Node)),
-    case RecType of
-        any -> %% #any.field
-            %% TODO: write here
-            Node;
-        _OtherRecType ->
-            Mode
-    end.
-
 maybe_optional_argument([]) -> undefined;
 maybe_optional_argument([X]) -> X.
-
-
-decode_record_names(RecNames) ->
-    %% Read a list of atoms.
-    [erl_syntax:atom_value(Node) || Node <- erl_syntax:list_elements(RecNames)].
 
 
 -spec is_local_function(Node, FunName) -> boolean() when
@@ -67,5 +40,102 @@ is_local_function(Node, FunName) ->
 always(_) -> true.
 
 node_type(Node) -> erl_syntax:type(Node).
+
+
+%% ----------------------------------------------------------------------
+%% Search and transform the record actions with `#any{}'.
+%% ----------------------------------------------------------------------
+
+%% Result of this fun used with erl_syntax_lib:map/2.
+-spec search_and_transform_record_action_any(RecNames, AltAction) -> GenFun when
+    RecNames :: [Tree],
+    AltAction :: Tree,
+    GenFun :: fun((Tree) -> Tree),
+    Tree :: erl_syntax:syntaxTree().
+    
+search_and_transform_record_action_any(RecNames, AltAction) ->
+    fun(Node) ->
+            case node_type(Node) of
+                record_expr -> %% X#node{f1 = Y}
+                    handle_record_expr_any(RecNames, AltAction, Node);
+                record_access -> %% X#node.f1
+                    handle_record_access_any(RecNames, AltAction, Node);
+                _OtherType -> Node
+            end
+        end.
+
+
+%% ----------------------------------------------------------------------
+%% Handle the record expression with `#any{}'.
+%% ----------------------------------------------------------------------
+
+handle_record_expr_any(RecNames, AltAction, Node) ->
+    RecType = erl_syntax:atom_value(erl_syntax:record_expr_type(Node)),
+    case RecType of
+        any -> %% #any.field
+            Arg = erl_syntax:record_expr_argument(Node),
+            F = generate_case_body_for_record_expr_any(Node),
+            Clauses = generate_record_match(RecNames, AltAction, F),
+            erl_syntax:case_expr(Arg, Clauses);
+        _OtherRecType ->
+            Node
+    end.
+
+
+generate_case_body_for_record_expr_any(OldNode) ->
+    %% Arg#any{Fields}
+    Arg = erl_syntax:record_expr_argument(OldNode),
+    Fields = erl_syntax:record_expr_fields(OldNode),
+    fun(Name) ->
+            [erl_syntax:record_expr(Arg, Name, Fields)]
+        end.
+
+
+%% ----------------------------------------------------------------------
+%% Handle the record access with `#any{}'.
+%% ----------------------------------------------------------------------
+
+handle_record_access_any(RecNames, AltAction, Node) ->
+    RecType = erl_syntax:atom_value(erl_syntax:record_access_type(Node)),
+    case RecType of
+        any -> %% #any.field
+            Arg = erl_syntax:record_access_argument(Node),
+            F = generate_case_body_for_record_access_any(Node),
+            Clauses = generate_record_match(RecNames, AltAction, F),
+            erl_syntax:case_expr(Arg, Clauses);
+        _OtherRecType ->
+            Node
+    end.
+
+
+generate_case_body_for_record_access_any(OldNode) ->
+    %% Arg#any{Fields}
+    Arg = erl_syntax:record_access_argument(OldNode),
+    Field = erl_syntax:record_access_field(OldNode),
+    fun(Name) ->
+            [erl_syntax:record_access(Arg, Name, Field)]
+        end.
+
+
+-spec generate_record_match(RecNames, AltAction, BodyGen) -> Clauses when
+    RecNames :: [RecName],
+    AltAction :: Tree | undefined,
+    BodyGen :: fun((RecName) -> Tree),
+    RecName :: Tree,
+    Tree :: erl_syntax:syntaxTree(),
+    Clauses :: [Tree].
+
+generate_record_match(RecNames, AltAction, BodyGen) ->
+    CF = fun erl_syntax:clause/3,
+    Clauses = [CF([rec(Name)], none, BodyGen(Name)) || Name <- RecNames],
+    case AltAction of
+        undefined -> Clauses;
+        _ -> Clauses ++ [CF([erl_syntax:underscore()], none, [AltAction])]
+    end.
+
+
+%% ``#Name{}``.
+rec(Name) -> erl_syntax:record_expr(none, Name, []).
+
 
 
--- test/sangria_tests.erl
+++ test/sangria_tests.erl
@@ -28,3 +28,43 @@ set_label_or_error(L, X) ->
 set_label_or_undefined(L, X) ->
     with_any([node, edge], X#any{label = L}, undefined).
 
+
+
+-include_lib("eunit/include/eunit.hrl").
+
+-ifdef(TEST).
+
+set_label_after_test_() ->
+    [ ?_assertEqual(set_label_after(mike, #node{}), #node{label = mike})
+    , ?_assertEqual(set_label_after(mike_joe, #edge{}), #edge{label = mike_joe})
+    , ?_assertError({case_clause, node}, set_label_after(mike, node))
+    ].
+
+get_label_after_test_() ->
+    [ ?_assertEqual(get_label_after(#node{label = mike}), mike)
+    , ?_assertEqual(get_label_after(#edge{label = mike_joe}), mike_joe)
+    , ?_assertError({case_clause, node}, get_label_after(node))
+    ].
+
+set_label_or_ignore_test_() ->
+    [ ?_assertEqual(set_label_or_ignore(mike, #node{}), #node{label = mike})
+    , ?_assertEqual(set_label_or_ignore(mike_joe, #edge{}),
+                    #edge{label = mike_joe})
+    , ?_assertEqual(set_label_or_ignore(mike, node), node)
+    ].
+
+set_label_or_error_test_() ->
+    [ ?_assertEqual(set_label_or_error(mike, #node{}), #node{label = mike})
+    , ?_assertEqual(set_label_or_error(mike_joe, #edge{}), #edge{label = mike_joe})
+    , ?_assertError(bad_record, set_label_or_error(mike, node))
+    ].
+
+set_label_or_undefined_test_() ->
+    [ ?_assertEqual(set_label_or_undefined(mike, #node{}), #node{label = mike})
+    , ?_assertEqual(set_label_or_undefined(mike_joe, #edge{}),
+                    #edge{label = mike_joe})
+    , ?_assertEqual(set_label_or_undefined(mike, node), undefined)
+    ].
+
+-endif.
+
